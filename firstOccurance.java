f50b1ebf222d36513a51d907acfe0d216d987fc9
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-16 20:33:47 +0530

    Create `firstOccurance.java`

---
+++ firstOccurance.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public int strStr(String haystack, String needle)
+    {
+        int index = -1;
+
+        for(int i = 0 ; i <= haystack.length() - needle.length() ; i++)
+        {
+            if(haystack.substring(i, i + needle.length()).equals(needle))
+            {
+                index = i;
+                break;
+            }
+        }
+        
+        return index;
+    }
+}
