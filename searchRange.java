8cc9d5bd4d76c203fd907a977d1991c3cc5115ee
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-17 23:13:47 +0530

    Update `searchRange.java`

--- searchRange.java
+++ searchRange.java
@@ -1,3 +1,5 @@
+// Method 1
+
 class Solution
 {
     public int[] searchRange(int[] nums, int target)
@@ -12,6 +14,36 @@ class Solution
                 
                 if(positions[0] == -1)
                     positions[0] = i;
+            }
+        }
+
+        return positions;
+    }
+}
+
+// Method 2
+
+class Solution
+{
+    public int[] searchRange(int[] nums, int target)
+    {
+        int[] positions = {-1, -1};
+
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            if(nums[i] == target)
+            {               
+                positions[0] = i;
+                break;
+            }
+        }
+
+        for(int i = nums.length - 1 ; i >= 0 ; i--)
+        {
+            if(nums[i] == target)
+            {
+                positions[1] = i;
+                break;
             }
         }
 
