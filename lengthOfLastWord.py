9b5f38c7e4b66e9e88a9ceb4f849e6f8cfcc0e45
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-18 13:27:45 +0530

    Update `lengthOfLastWord.py`

--- lengthOfLastWord.py
+++ lengthOfLastWord.py
@@ -1,5 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         for word in s.split(' ')[:: -1]:
-            if ' ' not in word and len(word) != 0:
+            if len(word) != 0:
                 return len(word)
