982b81eaa4749c13386100f27eb0bf7157dbaf19
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-19 21:40:18 +0530

    Update `isListPalindrome.java`

--- isListPalindrome.java
+++ isListPalindrome.java
@@ -8,6 +8,9 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Solution 1 - Slower
+
 class Solution
 {
     private static String findNum(ListNode head)
@@ -37,6 +40,45 @@ class Solution
         String num = findNum(head);
         
         if(num.equals(reverseString(num, 0)))
+            return true;
+        
+        return false;
+    }
+}
+
+// Solution 2 - Faster
+
+class Solution
+{
+    private static String findNum(ListNode head)
+    {
+        String num = "";
+
+        ListNode currNode = head;
+        while(currNode != null)
+        {
+            num += currNode.val;
+            currNode = currNode.next;
+        }
+
+        return num;
+    }
+
+    private static boolean isStringPalindrome(String str, int index)
+    {
+        if(index == str.length())
+            return true;
+        
+        if(str.charAt(index) == str.charAt(str.length() - index - 1))
+            return isStringPalindrome(str, index + 1);
+        return false;
+    }
+
+    public boolean isPalindrome(ListNode head)
+    {
+        String num = findNum(head);
+
+        if(isStringPalindrome(num, 0))
             return true;
         
         return false;
