5e6e1ec60f1b2d681b2ee3257235f72a59535f5f
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-20 19:39:39 +0530

    Create `removeElements.java`

---
+++ removeElements.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution
+{
+    public ListNode removeElements(ListNode head, int val)
+    {
+        if(head == null)
+            return head;
+                
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prevNode = dummy, currNode = head;
+
+        while(currNode != null)
+        {
+            if(currNode.val == val)
+            {
+                prevNode.next = currNode.next;
+                currNode = prevNode.next;
+                continue;
+            }
+                        
+            currNode = prevNode.next.next;
+            prevNode = prevNode.next;
+        }
+
+        return dummy.next;
+    }
+}
