527418a31c3b30b95065ec49c279adb2ab4339d3
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-21 17:58:01 +0530

    Update `isAlphanumericPalindrome.java`

--- isAlphanumericPalindrome.java
+++ isAlphanumericPalindrome.java
@@ -1,3 +1,5 @@
+// Solution - 1
+
 class Solution
 {
     private static String reverse(String s, int index)
@@ -22,6 +24,33 @@ class Solution
         }
 
         if(newStr.equals(reverse(newStr, 0)))
+            return true;
+        
+        return false;
+    }
+}
+
+// Solution - 2
+
+class Solution
+{
+    public boolean isPalindrome(String s)
+    {
+        String newStr = "", reverseStr = "";
+        char ch;
+
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            ch = Character.toLowerCase(s.charAt(i));
+
+            if(Character.isLetterOrDigit(ch))
+            {
+                newStr += ch;
+                reverseStr = ch + reverseStr;
+            }
+        }
+
+        if(newStr.equals(reverseStr))
             return true;
         
         return false;
