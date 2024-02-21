b91fe39cdd716e0bad75404db4570373fb108050
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-21 18:48:14 +0530

    Update `validParenthese.java`

--- validParenthese.java
+++ validParenthese.java
@@ -1,3 +1,5 @@
+// Solution 1 - Without Implementing Stack (Slower)
+
 class Solution
 {
     public boolean isValid(String s)
@@ -84,6 +86,62 @@ class Solution
         }
 
         if(parentheses == 0 && square == 0 && curly == 0)
+            return true;
+        
+        return false;
+    }
+}
+
+// Solution 2 - Implementing Stack (faster)
+
+import java.util.Stack;
+
+class Solution
+{
+    public boolean isValid(String s)
+    {
+        Stack<Character> stack = new Stack<Character>();
+
+        for(char ch : s.toCharArray())
+        {
+            if(ch == '(' || ch == '[' || ch == '{')
+            {
+                stack.push(ch);
+                continue;
+            }
+
+            if(stack.isEmpty())
+                return false;
+            
+            switch(ch)
+            {
+                case ')':
+                    if(stack.peek() == '(')
+                        stack.pop();
+                    else
+                        return false;
+                    
+                    break;
+                    
+                case ']':
+                    if(stack.peek() == '[')
+                        stack.pop();
+                    else
+                        return false;
+                    
+                    break;
+                    
+                case '}':
+                    if(stack.peek() == '{')
+                        stack.pop();
+                    else
+                        return false;
+                    
+                    break;
+            }
+        }
+
+        if(stack.isEmpty())
             return true;
         
         return false;
