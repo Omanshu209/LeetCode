90966239ec774e827601a0e62d96ef324e6a79c8
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-23 18:37:58 +0530

    Update `isSameTree.java`

--- isSameTree.java
+++ isSameTree.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+
 class Solution
 {
     public boolean isSameTree(TreeNode p, TreeNode q)
@@ -21,8 +22,7 @@ class Solution
             return true;
         else if(p == null || q == null)
             return false;
-        
-        if(p.val != q.val)
+        else if(p.val != q.val)
             return false;
         
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
