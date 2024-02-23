3761fd488350f95682c6de3137226047e79e62ed
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-23 21:03:40 +0530

    Update `reverseWords.py`

--- String/reverseWords.py
+++ String/reverseWords.py
@@ -1,7 +1,20 @@
+# Solution 1 - Slower
+
 class Solution:
     def reverseWords(self, s: str) -> str:
         newStr = ''
         for word in s.split(' ')[::-1]:
+            if word != '':
+                newStr += word + ' '
+        return newStr[:-1]
+
+# Solution 2 - Faster
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        newStr = ''
+        words = s.split(' ')[::-1]
+        for word in words:
             if word != '':
                 newStr += word + ' '
         return newStr[:-1]
