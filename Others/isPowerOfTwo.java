336c4501dd77c02a2da826a05cfce83c677e2f16
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-24 19:53:08 +0530

    Create `isPowerOfTwo.java`

---
+++ Others/isPowerOfTwo.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public boolean isPowerOfTwo(int n)
+    {
+        for(int i = 0 ; true ; i++)
+        {
+            if(Math.pow(2, i) == n)
+                return true;
+            else if(Math.pow(2, i) > n)
+                break;
+        }
+
+        return false;
+    }
+}
