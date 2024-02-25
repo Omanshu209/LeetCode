ecb528bf279a1a7a1837b1317416168d85ca05e6
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-25 17:50:48 +0530

    Update `isHappy.java`

--- Hash Table/isHappy.java
+++ Hash Table/isHappy.java
@@ -1,3 +1,5 @@
+// Solution 1 (Using HashMap)
+
 import java.util.HashMap;
 
 class Solution
@@ -21,6 +23,39 @@ class Solution
                 return false;
             else
                 map.put(sum, 1);
+
+            n = sum;
+        }
+
+        return true;
+    }
+}
+
+// Solution 2 (Using HashSet) - uses lesser memory
+
+import java.util.HashSet;
+
+class Solution
+{
+    public boolean isHappy(int n)
+    {
+        HashSet<Integer> set = new HashSet<Integer>();
+        int sum;
+
+        while(n != 1)
+        {
+            sum = 0;
+
+            while(n > 0)
+            {
+                sum += Math.pow(n % 10, 2);
+                n /= 10;
+            }
+
+            if(set.contains(sum))
+                return false;
+            else
+                set.add(sum);
 
             n = sum;
         }
