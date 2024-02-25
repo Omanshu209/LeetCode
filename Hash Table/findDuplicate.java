7381c520e2ca004c3b98623b3ad832126c34e682
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-25 19:15:17 +0530

    Create `findDuplicate.java`

---
+++ Hash Table/findDuplicate.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+class Solution
+{
+    public int findDuplicate(int[] nums)
+    {
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        for(int num : nums)
+        {
+            if(set.contains(num))
+                return num;
+            set.add(num);
+        }
+
+        return -1;
+    }
+}
