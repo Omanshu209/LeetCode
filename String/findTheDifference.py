b05a868242c3a43bd7d994e62d3af3b44c756f17
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-03-04 19:48:21 +0530

    Create `findTheDifference.py`

---
+++ String/findTheDifference.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        chars: dict = {}
+
+        for char in s:
+            if char not in chars:
+                chars[char] = 1
+                continue
+            chars[char] += 1;
+        
+        for char in t:
+            if char not in chars:
+                return char
+            elif chars[char] == 0:
+                return char
+            chars[char] -= 1
