5ec510f39e758476ed3df852cf82d7021b7a6393
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-03-09 13:35:39 +0530

    Update `nth_highest_salary.py`

--- Pandas/nth_highest_salary.py
+++ Pandas/nth_highest_salary.py
@@ -1,3 +1,5 @@
+# Solution 1
+
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
@@ -12,5 +14,22 @@ def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
     
     return pd.DataFrame(
         [salary_sorted.iloc[N - 1]], 
+        columns = [f"getNthHighestSalary({N})"]
+    )
+
+# Solution 2
+
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    salary_sorted = employee.salary.sort_values(ascending = False)
+    salary_sorted.drop_duplicates(inplace = True)
+
+    val = None
+    if not (len(salary_sorted) < N or N <= 0):
+        val = salary_sorted.iloc[N - 1]
+
+    return pd.DataFrame(
+        [val], 
         columns = [f"getNthHighestSalary({N})"]
     )
