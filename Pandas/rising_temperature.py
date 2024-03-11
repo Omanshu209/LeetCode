20d3023a895782800a5bb9af5eccd0ed1a5e3b84
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-03-11 22:08:41 +0530

    Create `rising_temperature.py`

---
+++ Pandas/rising_temperature.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from datetime import timedelta
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather.recordDate = pd.to_datetime(weather.recordDate)
+    weather = weather.sort_values(by = "recordDate", ascending = True)
+
+    data = []
+    numpy_arr = weather.values
+    for i, row in enumerate(numpy_arr):
+        if i == 0:
+            continue
+        print(row[1]+timedelta(days = 1))
+        if row[2] > numpy_arr[i - 1][2] and numpy_arr[i - 1][1] + timedelta(days = 1) == row[1]:
+            data.append(row[0])
+    
+    return pd.DataFrame(data, columns = ["id"])
