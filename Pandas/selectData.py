20578fa64ec29cc591ae6a7c14cd81fa62ff481f
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-03-17 14:12:22 +0530

    Update `selectData.py`

--- Pandas/selectData.py
+++ Pandas/selectData.py
@@ -1,7 +1,26 @@
+# Solution 1
+
 import pandas as pd
 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
     return students[students.student_id == 101].drop(
         "student_id", 
         axis = "columns"
+    )
+
+# Solution 2 
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    data = []
+    np_arr = students.values
+
+    for row in np_arr:
+        if row[0] == 101:
+            data.append([row[1], row[2]])
+    
+    return pd.DataFrame(
+        data, 
+        columns = students.columns[1:]
     )
