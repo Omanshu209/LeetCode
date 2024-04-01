7292ca8d2cd19f3a4185f9e9a36573b77a0f7dbe
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-04-01 18:55:03 +0530

    Create `findCenter.java`

---
+++ Graph/findCenter.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    public int findCenter(int[][] edges)
+    {
+        int n = 1;
+        for(int[] edge : edges)
+            for(int v : edge)
+                if(v > n)
+                    n = v;
+        
+        int[][] adMatrix = new int[n + 1][n + 1];
+        
+        for(int[] edge : edges)
+        {
+            adMatrix[edge[0]][edge[1]] = 1;
+            adMatrix[edge[1]][edge[0]] = 1;
+        }
+
+        for(int i = 0 ; i <= n ; i++)
+        {
+            int edgeCount = 0;
+
+            for(int val : adMatrix[i])
+                edgeCount += val;
+            
+            if(edgeCount + 1 == n)
+                return i;
+        }
+
+        return -1;
+    }
+}
