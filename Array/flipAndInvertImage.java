ae65889f244c9124af741e3d23e89cfce33d8a3e
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-08-11 14:42:13 +0530

    Update `flipAndInvertImage.java`

--- Array/flipAndInvertImage.java
+++ Array/flipAndInvertImage.java
@@ -1,3 +1,5 @@
+// Solution 1
+
 class Solution
 {
     public int[][] flipAndInvertImage(int[][] image)
@@ -15,6 +17,26 @@ class Solution
         for(int i = 0 ; i < image.length ; i++)
             for(int j = 0 ; j < image[0].length ; j++)
                 image[i][j] = 1 - image[i][j];
+        
+        return image;
+    }
+}
+
+// Solution 2
+
+class Solution
+{
+    public int[][] flipAndInvertImage(int[][] image)
+    {
+        for(int i = 0 ; i < image.length ; i++)
+        {
+            for(int j = 0 ; j < Math.ceil(image[0].length / 2.0) ; j++)
+            {
+                int temp = image[i][j];
+                image[i][j] = 1 - image[i][image[0].length - j - 1];
+                image[i][image[0].length - j -1] = 1 - temp;
+            }
+        }
         
         return image;
     }
